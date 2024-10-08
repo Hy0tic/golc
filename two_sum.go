091192536c12a349a22c360e7ff83e778775267e
Hy0tic <vothanhdat278@gmail.com>
2024-10-08 04:08:54 -0500

    init commit

---
+++ two_sum.go
@@ -0,0 +1,17 @@
+package main
+
+func twoSum(nums []int, target int) []int {
+    var prevMap = make(map[int]int) // create dictionary of value and index
+
+	for i := 0; i < len(nums); i++ {
+		var checkThisNumber = target - nums[i]
+
+		if _, exists := prevMap[checkThisNumber]; exists {
+			return []int{prevMap[checkThisNumber], i}
+		} else {
+			prevMap[nums[i]] = i
+		}
+	}
+
+	return nums
+}
---
+++ valid_anagram.go
@@ -0,0 +1,36 @@
+package main
+
+func isAnagram(s string, t string) bool {
+    map1 := map[byte]int{}
+	map2 := map[byte]int{}
+
+	for i := 0; i < len(s); i++{
+		map1[s[i]]++
+	}
+
+	for i := 0; i < len(t); i++{
+		map2[t[i]]++
+	}
+
+    // Iterate through the first map
+    for key, value1 := range map1 {
+        if value2, exists := map2[key]; exists {
+            // Compare values from both maps
+            if value1 == value2 {
+                continue 
+            } else {
+                return false
+            }
+        } else {
+            return false
+        }
+    }
+
+	for key := range map2 {
+        if _, exists := map1[key]; !exists {
+            return false
+        }
+    }
+
+	return true
+}
