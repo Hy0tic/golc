f892d2c30fc7288167b6cf40394e06d45ac26235
Hy0tic <vothanhdat278@gmail.com>
2024-11-19 10:42:32 -0600

    lc 424

---
+++ solutions/neetcode150/3.SlidingWindow/longest_repeating_character_replacement.go
@@ -0,0 +1,31 @@
+package main
+
+// leetcode 424
+func characterReplacement(s string, k int) int {
+    left := 0
+    maxFreq := 0
+    result := 0
+    freqMap := make(map[byte]int)
+
+    for right := 0; right < len(s); right++ {
+        freqMap[s[right]]++
+        maxFreq = max(maxFreq, freqMap[s[right]])
+
+        // Shrink the window if the number of replacements exceeds k
+        for right-left+1-maxFreq > k {
+            freqMap[s[left]]--
+            left++
+        }
+
+        result = max(result, right-left+1)
+    }
+
+    return result
+}
+
+func max(a, b int) int {
+    if a > b {
+        return a
+    }
+    return b
+}
