213778290dc610a319b5903d182c14cd455cf720
Hy0tic <vothanhdat278@gmail.com>
2025-01-31 21:25:08 -0600

    update lc 3

--- solutions/neetcode150/3.SlidingWindow/longest_substring_without_repeating.go
+++ solutions/neetcode150/3.SlidingWindow/longest_substring_without_repeating.go
@@ -2,44 +2,23 @@ package main
 
 // leetcode #3
 func lengthOfLongestSubstring(s string) int {
-    l := 0
+	l := 0
 	r := 1
-
 	maxLen := 0
+	strMap := make(map[byte]bool)
 
-	for r < len(s) + 1 {
-		substring := s[l : r]
-		strLen := len(substring)
-
-		// if there are no repeating character
-		if strLen == uniqueCharacterCount(substring) { 
-			maxLen = max(maxLen, strLen)
-		} else {
+	for r < len(s) {
+		// while substring contains duplicate, move L up
+		for strMap[s[r]] {
+			strMap[s[l]] = false
 			l++
 		}
+
+		// substring no longer contain duplicate/substring does not contain duplicate
+		strMap[s[r]] = true
+		maxLen = Max(maxLen, r-l+1)
 		r++
 	}
 
 	return maxLen
-}
-
-func uniqueCharacterCount(s string) int {
-	// Create a map to track unique characters
-	charMap := make(map[rune]bool)
-
-	// Loop through each character in the string
-	for _, char := range s {
-		charMap[char] = true // Mark the character as seen
-	}
-
-	// The number of unique characters is the size of the map
-	return len(charMap)
-}
-
-func max(a, b int) int {
-	if a > b {
-		return a
-	} else {
-		return b
-	}
 }
