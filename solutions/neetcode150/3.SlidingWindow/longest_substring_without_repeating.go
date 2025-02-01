1ea3544a06af0bb1546714588a601d92cb8648e8
Hy0tic <vothanhdat278@gmail.com>
2025-01-31 21:33:40 -0600

    update lc 3

--- solutions/neetcode150/3.SlidingWindow/longest_substring_without_repeating.go
+++ solutions/neetcode150/3.SlidingWindow/longest_substring_without_repeating.go
@@ -2,21 +2,17 @@ package main
 
 // leetcode #3
 func lengthOfLongestSubstring(s string) int {
-	l := 0
-	r := 1
-	maxLen := 0
+	l, r, maxLen := 0, 0, 0
 	strMap := make(map[byte]bool)
 
 	for r < len(s) {
-		// while substring contains duplicate, move L up
 		for strMap[s[r]] {
 			strMap[s[l]] = false
 			l++
 		}
 
-		// substring no longer contain duplicate/substring does not contain duplicate
 		strMap[s[r]] = true
-		maxLen = Max(maxLen, r-l+1)
+		maxLen = Max(maxLen, r - l + 1)
 		r++
 	}
 
