ccb890592df02959b6a369bf062e43c7800b62be
Hy0tic <vothanhdat278@gmail.com>
2025-02-02 09:38:50 -0600

    update 567

--- solutions/neetcode150/3.SlidingWindow/permutation_in_string.go
+++ solutions/neetcode150/3.SlidingWindow/permutation_in_string.go
@@ -3,51 +3,52 @@ package main
 
 // leetcode #567
 func checkInclusion(s1 string, s2 string) bool {
-    if len(s2) < len(s1) {
-        return false
-    }
+	if len(s2) < len(s1) {
+		return false
+	}
 
-    // Initialize frequency maps for s1 and the current window in s2
-    s1Freq := make([]int, 26)
-    s2Freq := make([]int, 26)
+	l, r, matches := 0, len(s1), 0
+	
+	s1freq, s2freq := [26]int{}, [26]int{}
 
-    for i := 0; i < len(s1); i++ {
-        s1Freq[s1[i]-'a']++
-        s2Freq[s2[i]-'a']++
-    }
+	for i := 0; i < len(s1); i++ {
+		s1freq[s1[i] - byte('a')]++
+		s2freq[s2[i] - byte('a')]++
+	}
 
-    matches := 0
-    for i := 0; i < 26; i++ {
-        if s1Freq[i] == s2Freq[i] {
-            matches++
-        }
-    }
+	for i := 0; i < 26; i++ {
+		if s1freq[i] == s2freq[i] {
+			matches++
+		}
+	}
 
-    left := 0
-    for right := len(s1); right < len(s2); right++ {
-        if matches == 26 {
-            return true
-        }
+	for r < len(s2) {
+		if matches == 26 {
+			return true
+		}
+        
+		// add new character from the right
+		index := s2[r] - byte('a')
+		s2freq[index]++
+		if s1freq[index] == s2freq[index]{
+			matches++
+		} else if s1freq[index] + 1 == s2freq[index] {
+			matches--
+		}
 
-        // Add the next character to the window
-        index := s2[right] - 'a'
-        s2Freq[index]++
-        if s1Freq[index] == s2Freq[index] {
-            matches++
-        } else if s1Freq[index]+1 == s2Freq[index] {
-            matches--
-        }
+		// remove character from the left
+		index = s2[l] - byte('a')
+		s2freq[index]--
+		if s1freq[index] == s2freq[index]{
+			matches++
+		} else if s1freq[index]-1 == s2freq[index] {
+			matches--
+		}
 
-        // Remove the left character from the window
-        index = s2[left] - 'a'
-        s2Freq[index]--
-        if s1Freq[index] == s2Freq[index] {
-            matches++
-        } else if s1Freq[index]-1 == s2Freq[index] {
-            matches--
-        }
-        left++
-    }
+		l++
+		r++
+	}
 
-    return matches == 26
+	return matches == 26
+
 }
